0c59b7416695401318aadf5e2509e71d380ca1fc
CristopherAmaral <cristopher.amaral@live.com>
2024-09-04 22:46:37 -0300

    Inicio do projeto

---
+++ ContaBanco/src/main/java/com/cristopheramaral/contabanco/ContaTerminal.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.cristopheramaral.contabanco;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MeuPerfil
+ */
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+
+        int numero = 0;
+        String agencia = "SEMAGENCIA";
+        String nomeCliente = "SEMCLIENTE";
+        Double saldo = 0.0D;
+        Scanner valorInformado = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta !");
+        numero = valorInformado.nextInt();
+        
+        System.out.println("Por favor digite o numero da Agência !");
+        agencia = valorInformado.nextLine();
+        
+        System.out.println("Por favor digite o nome do Cliente");
+        nomeCliente = valorInformado.nextLine();
+        
+        System.out.println("Por favor digite o Saldo da Conta");
+        saldo = valorInformado.nextDouble();
+                        
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco,"
+                + "sua agência é "+agencia+", conta"+numero+"e seu saldo "+saldo
+                +" já está disponível para saque.");
+        
+        
+    }
+
+}
